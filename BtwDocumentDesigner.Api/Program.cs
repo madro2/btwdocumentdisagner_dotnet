ccecb3794895947dbb2e39ef240fbd05e15cc43d
Daniel Madroñero <ing3soporte@btw.com.co>
2026-07-18 21:20:30 -0500

    Revert CORS configuration in favor of frontend proxy

--- BtwDocumentDesigner.Api/Program.cs
+++ BtwDocumentDesigner.Api/Program.cs
@@ -24,17 +24,6 @@ builder.Services.AddScoped<IDesignService, DesignService>();
 builder.Services.AddScoped<IImageService, ImageService>();
 builder.Services.AddScoped<IGeneratorService, GeneratorService>();
 
-// Configure CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAngularApp", policy =>
-    {
-        policy.WithOrigins("http://localhost:4200")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -45,8 +34,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseCors("AllowAngularApp");
 
 // Map ASP.NET Core Controllers
 app.MapControllers();
